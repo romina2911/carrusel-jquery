31d909dd11426080262b9c68d94c20096fe8670a
romina2911 <romina.hp@gmail.com>
2017-12-22 02:03:29 -0500

    subiendo cambios de html, css y javascript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
+$(document).ready(loadPage);
+
+    var target = 0;
+
+    var loadPage = function () {
+
+        //Elementos
+
+        var $buttons = $(".control");
+        var $previous = $(".previous");
+        var $next = $(".next");
+
+        //Eventos
+
+        $buttons.click(changeImage);
+        $previous.click(previousImage);
+        $next.click(nextImage);
+    
+    };
+
+    var changeImage = function () {
+        target = parseInt($(this).data("target"));
+        showImage(target);
+    };
+
+    var previousImage = function(e) {
+        e.preventDefault();
+        target = target -1;
+        target = (target < 0) ? 4 : target;
+        showImage(target);
+    }
+
+    var nextImage = function(e) {
+        e.preventDefault();
+        target = target + 1;
+        target = (target > 4) ? 0 : target;
+        showImage(target);
+    }
 
 
-$(document).ready(function() {
 
-    $('.slides li').hide();
-    $('.slides li:first').show();
-
-
-
-});
